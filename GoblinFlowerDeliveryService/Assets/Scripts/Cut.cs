dfca7486909fccafe4c1a08ba6c39aeef0d25347
zuran <gsugrader@gmail.com>
2020-09-20 21:04:15 -0700

    Garden scene placeholder with scissors

---
+++ GoblinFlowerDeliveryService/Assets/Scripts/Cut.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Cut : MonoBehaviour
+{
+    public GameObject Scissors;
+    private Animator m_Animator;
+    private AudioSource m_Audio;
+
+    private List<UnityEngine.XR.InputDevice> rightHandDevices;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
+        m_Animator = Scissors.GetComponent<Animator>();
+        m_Audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rightHandDevices.Count > 0) {
+            var contr = rightHandDevices[0];
+            bool triggerValue;
+            bool isIdle = m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Idle");
+            if(isIdle && contr.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue) {
+                Debug.Log("CUT");
+                m_Animator.SetTrigger("BeginCut");
+                m_Audio.Play();
+            } else {
+                m_Animator.ResetTrigger("BeginCut");
+            }
+
+        }
+    }
+}
