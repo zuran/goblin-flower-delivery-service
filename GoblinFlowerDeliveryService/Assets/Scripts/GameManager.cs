eea98f9bebd072c5855a63cff62b23932585755a
zuran <gsugrader@gmail.com>
2020-09-23 22:39:21 -0700

    Game manager with clue generation

---
+++ GoblinFlowerDeliveryService/Assets/Scripts/GameManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Text;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    // Quest alerts
+    public Canvas QuestAlertCanvas;
+    public TextMeshProUGUI RiddleComponent;
+    public TextMeshProUGUI AddressComponent;
+
+    public enum Materials
+    {
+        FlyAgaric,
+        Tulip,
+        Buttercup,
+        MorningGlory,
+        Morel,
+        Enoki,
+        Cactus
+    }
+
+    public List<Materials> _correctArrangement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ClickMailbox()
+    {
+        ActivateNewQuest();
+        QuestAlertCanvas.enabled = true;
+    }
+
+    public void CloseQuestWindow()
+    {
+        QuestAlertCanvas.enabled = false;
+    }
+
+    // Quest alert helpers
+    private void ActivateNewQuest()
+    {
+        AddressComponent.SetText(GenerateAddress());
+        RiddleComponent.SetText(GenerateRiddle());
+    }
+    private string GenerateRiddle()
+    {
+        _correctArrangement = new List<Materials>();
+
+        var answers = new Materials[]
+        {
+            Materials.FlyAgaric,
+            Materials.Tulip,
+            Materials.Buttercup,
+            Materials.MorningGlory,
+            Materials.Morel,
+            Materials.Enoki,
+            Materials.Cactus
+        };
+
+        var clues = new string[] {
+            "surprisingly grounded, mottled, and rounded",
+            "the bulb doesn't glow but from it this grows",
+            "yellow blooming weeds fit for a princess",
+            "best before night, may bloom at first light(this is terrible i know)",
+            "a winsome ridged treat you'll be tempted to eat",
+            "worthy of a pallid ballad, soup, or salad",
+            "a prickly pear for the adventurous one" };
+
+        var riddle = new StringBuilder("Your arrangment must include ingredients matching these descriptions\n");
+        var rand = new System.Random();
+        var used = new List<int>();
+        for (int i = 0; i < 3; i++)
+        {
+            int index = -1;
+            do
+            {
+                index = rand.Next(0, clues.Length);
+            } while (used.Contains(index));
+            used.Add(index);
+            riddle.Append(clues[index] + "\n");
+            _correctArrangement.Add(answers[i]);
+        }
+
+        var answerString = new StringBuilder("Answer: ");
+        foreach(var i in used)
+        {
+            answerString.Append(answers[i].ToString() + ", ");
+        }
+        Debug.Log(answerString.ToString());
+
+        return riddle.ToString();
+    }
+
+    private string GenerateAddress()
+    {
+        var addresses = new string[] { "1020", "1021", "1022", "4041", "4042", "4043", "4044" };
+        var index = new System.Random().Next(0, addresses.Length);
+        return addresses[index];
+    }
+
+}
